aa2bf8acfadf00cd18f0944b2a631dc7eb19101b
impulse <2535189010@qq.com>
2023-02-03 23:07:45 +0800

    钩子移动逻辑

--- 项目/GBTGame/Assets/Scripts/UI/UIForm/HandleForm.cs
+++ 项目/GBTGame/Assets/Scripts/UI/UIForm/HandleForm.cs
@@ -7,6 +7,15 @@ using UnityEngine.UI;
 //CreateTime：2023/2/3 19:39:36
 public partial class HandleForm : UIForm
 {
+	public float AngleOffset = 0;
+	public float HandleAngleArea = 180;
+	public float HandleMoveSpeed = 100;
+	public float CurRealAngle => m_CurAngle + AngleOffset;
+
+	private float m_CurAngle = 0;
+	private bool m_IsRight = true;
+	private bool m_IsHandleMoving = false;
+
 	public override void Awake()
 	{
 		base.Awake();
@@ -19,7 +28,52 @@ public partial class HandleForm : UIForm
 		RegisterEvent(); 
 	}
 
-	public override void OnClose()
+    public override void Update()
+    {
+        base.Update();
+
+		if (m_IsHandleMoving)
+        {
+
+        }
+        else
+        {
+			if (Input.GetKeyDown(KeyCode.S))
+			{
+				m_IsHandleMoving = true;
+			}
+			else
+			{
+				UpdateHandleAngle();
+			}
+		}
+	}
+
+	private void UpdateHandleAngle()
+    {
+		if (m_IsRight)
+		{
+			m_CurAngle += HandleMoveSpeed * Time.deltaTime;
+		}
+		else
+		{
+			m_CurAngle -= HandleMoveSpeed * Time.deltaTime;
+		}
+
+		if (m_CurAngle > HandleAngleArea)
+		{
+			m_IsRight = false;
+		}
+		else if (m_CurAngle < 0)
+		{
+			m_IsRight = true;
+		}
+
+		m_CurAngle = Mathf.Clamp(m_CurAngle, 0, HandleAngleArea);
+		m_rect_arrow.localEulerAngles = new Vector3(0, 0, CurRealAngle);
+	}
+
+    public override void OnClose()
 	{
 		base.OnClose();
 		ReleaseEvent(); 
@@ -32,9 +86,7 @@ public partial class HandleForm : UIForm
 
 	private void ReleaseEvent()
 	{
-		
+
 	}
-
-
 }
 
--- 项目/GBTGame/Assets/Scripts/UI/UIFormBind/HandleForm.Bind.cs
+++ 项目/GBTGame/Assets/Scripts/UI/UIFormBind/HandleForm.Bind.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 using MyGameFrameWork;
 using UnityEngine.UI;
 
-//CreateTime：2023/2/3 19:39:36
+//CreateTime��2/3/2023 9:55:11 PM
 public partial class HandleForm
 {
 	private AutoBind autoBind;
-	private Image m_imgHandle;
+	private RectTransform m_rect_arrow;
 
 	private void InitComponent()
 	{
 		autoBind = GetComponent<AutoBind>();
-		m_imgHandle = autoBind.itemList[0].obj.GetComponent<Image>();
+		m_rect_arrow = autoBind.itemList[0].obj.GetComponent<RectTransform>();
 	}
 }
 
